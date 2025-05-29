6ce75102dd3cb9459c570cc32763ffea712978a8
Parsa <parsakhorram@gmail.com>
2025-05-30 00:06:19 +0330

    Finishing Phase 3

--- Online/GameState.java
+++ Online/GameState.java
@@ -1,12 +1,4 @@
 package Online;
 
-import java.io.Serializable;
-import java.awt.Point;
-
-public class GameState implements Serializable {
-    public Point playerPosition;
-
-    public GameState(Point playerPosition) {
-        this.playerPosition = playerPosition;
-    }
+public class GameState {
 }
